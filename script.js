1d63fd2aca7b52ca56b78d2f054a3f317ec418ba
KennGD <166850728+KennGD@users.noreply.github.com>
2024-04-13 15:10:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<PRE>
+<SCRIPT LANGUAGE="JavaScript">
+document.writeln("This is the first line in my JavaScript file.");
+document.writeln("This is the second line in my JavaScript file.");
+  
+</SCRIPT>
+</PRE>
+
